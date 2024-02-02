408669950b90536852e7206e9bbf356c92716917
areddy2022 <areddyyt@gmail.com>
2024-02-02 11:35:41 -0500

    Fix all errors

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let password = document.getElementById("#password");
-let confirmPassword = document.querySelector("confirm-password");
+const confirmPasswordField = document.getElementById("confirm-password");
+const passwordField = document.getElementById("password");
+
+let password;
+let confirmPassword;
+
+passwordField.onblur = function() {
+    password = passwordField.value;
+}
+
+confirmPasswordField.onblur = function() {
+    confirmPassword = confirmPasswordField.value;
+    if(password != confirmPassword){
+        confirmPasswordField.setCustomValidity("Please enter matching passwords.");
+    }
+    else{
+        confirmPassword.setCustomValidity("");
+    }
+}
